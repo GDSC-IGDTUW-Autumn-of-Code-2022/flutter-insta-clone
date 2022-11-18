68eac129644225d9d35d618256ed5e8b035375cf
Namya13Jain <100767035+Namya13Jain@users.noreply.github.com>
2022-11-18 18:35:15 +0530

    Updated accounts screen

--- app/lib/screens/account.dart
+++ app/lib/screens/account.dart
@@ -10,9 +10,66 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Account'),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        leading: const Icon(Icons.lock),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text('abc_123'),
+            Row(
+              children: const [
+                Icon(Icons.arrow_drop_down_rounded),
+                SizedBox(
+                  width: 110,
+                ),
+                Icon(Icons.add),
+                SizedBox(
+                  width: 20,
+                ),
+                Icon(Icons.table_rows_rounded),
+                SizedBox(
+                  width: 20,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      body: Row(
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.fromLTRB(20, 20, 20, 40),
+            child: const CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.grey,
+            ),
+          ),
+          const Text(
+            "Posts",
+            style: TextStyle(fontSize: 15),
+          ),
+          const SizedBox(
+            width: 15,
+          ),
+          const Text(
+            "Followers",
+            style: TextStyle(
+              fontSize: 15,
+            ),
+          ),
+          const SizedBox(
+            width: 15,
+          ),
+          const Text(
+            "Following",
+            style: TextStyle(
+              fontSize: 15,
+            ),
+          ),
+        ],
       ),
     );
   }
